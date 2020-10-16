2501eab15b0ca7af847f84bdb2a6a6f25ea02745
Paul Larson <kj6863@go.minnstate.edu>
2020-10-16 12:58:41 -0500

    I don't think I am doing this right

--- src/Tavunu.java
+++ src/Tavunu.java
@@ -7,8 +7,103 @@
  * pava -- items of status used for bargaining.
  *
  * @author Mithat Konar
- * @author Your Name
+ * @author Paul Larson
  */
 public class Tavunu {
-    /* See readme.md for what to do. */
+    private String name;
+    private int birthdate;
+    private int pavaamnt;
+    
+    public Tavunu()
+    {
+        name = "";
+        birthdate = Integer.MIN_VALUE;
+        pavaamnt = 0;
+    }
+    Tavunu(String a, int b, int c)
+    {
+        name = a;
+        birthdate = b;
+        pavaamnt = c; 
+    }
+    
+
+    public Boolean setName(String newname)
+    {
+        Boolean nameright = true;
+        char first;
+        if(!newname.isEmpty())
+        {
+            first = (newname.charAt(0));
+            
+            if((first == ('T')) || (first == ('D')))
+            {
+                name = newname;
+                return nameright;
+            
+            }
+            else
+            {
+                nameright = false;
+                return nameright;
+            }
+        }
+        else{
+            nameright = false;
+            return nameright;
+        }
+    }
+        
+    public String getName()
+    {
+        return name;
+    }
+    public Boolean spendPava(int amount)
+    {
+        Boolean spend = true;
+        if(amount < 0)
+        {
+            spend = false;
+            return spend;
+        }
+        else
+        {
+            pavaamnt -= amount;
+            spend = true;
+            return spend;
+        }
+
+    }
+    public Boolean receivePava(int amount)
+    {
+        Boolean give = true;
+        if(amount < 0)
+        {
+            give = false;
+            return give;
+        }
+        else
+        {
+            pavaamnt += amount;
+            give = true;
+            return give;
+        }
+ 
+    }
+    public int getPava()
+    {
+        return pavaamnt;
+    }
+    public void setBirthYear(int date)
+    {
+        birthdate = date;
+    }
+    public int getBirthYear()
+    {
+        return birthdate;
+    }
+    public String toString()
+    {
+        return (name + " born in " + birthdate + " has " + pavaamnt + " pava." );
+    }
 }
