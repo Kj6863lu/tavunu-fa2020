caa8ec311b101107ed2815645c830503bd985eb5
Kj6863lu <72282857+Kj6863lu@users.noreply.github.com>
2020-10-16 11:31:32 -0500

    Add files via upload
    I think this is working, I figured out what I was doing wrong with the constructors at least!

--- src/Tavunu.java
+++ src/Tavunu.java
@@ -7,8 +7,79 @@
  * pava -- items of status used for bargaining.
  *
  * @author Mithat Konar
- * @author Your Name
+ * @author Paul Larson
  */
 public class Tavunu {
-    /* See readme.md for what to do. */
+    private String name;
+    private int birthdate;
+    private int pavaamnt;
+    
+    public Tavunu()
+    {
+        name = "";
+        birthdate = 0;
+        pavaamnt = 0;
+    }
+    Tavunu(String a, int b, int c)
+    {
+        name = a;
+        birthdate = b;
+        pavaamnt = c; 
+    }
+    
+    public void Tavunu()
+    {
+        
+    }
+   
+    public Boolean setName(String name)
+    {
+       
+        {
+            
+        }
+        return true;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public Boolean spendPava(int amount)
+    {
+        if(pavaamnt > 0)
+        {
+            pavaamnt = pavaamnt - amount;
+            return true;
+        }
+        return false;
+
+    }
+    public Boolean receivePava(int amount)
+    {
+        if(pavaamnt <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            pavaamnt += amount;
+            return true;
+        }
+    }
+    public int getPava()
+    {
+        return pavaamnt;
+    }
+    public void setBirthYear(int date)
+    {
+        birthdate = date;
+    }
+    public int getBirthYear()
+    {
+        return birthdate;
+    }
+    public String toString()
+    {
+        return (name + pavaamnt + birthdate);
+    }
 }
